ac64f447ccf9536d1a6089c3dcd8a9814146c293
Rakesh Joshi <joshirakesh165@gmail.com>
2025-05-20 10:15:20 +0530

    Update fetchWithRetry.js

--- fetchWithRetry.js
+++ fetchWithRetry.js
@@ -1,1 +1,20 @@
+function fetchWithAutoRetry(fetcher, maximumRetryCount) {
+  // your code here
 
+  let count = 0;
+
+  async function helper() {
+    try {
+      return await fetcher();
+    } catch (e) {
+      count++
+      if(count <= maximumRetryCount) {
+        return helper();
+      } else {
+        return Promise.reject(e);
+      }
+    }
+  }
+  return helper();
+
+}
